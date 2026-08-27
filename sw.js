0a1595d952414600ec5f2c8f3f7e620b5508a8d8
jonasdiaz-netizen <jonas.diaz@hotmail.de>
2026-08-27 16:10:04 +0200

    Bump PWA cache for growth KPIs

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="portfolio-os-shell-v11-2";
+const CACHE="portfolio-os-shell-v12-growth";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
