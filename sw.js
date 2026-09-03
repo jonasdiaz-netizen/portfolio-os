6316d7b6adbe5e034601b8d0b5159f2980b882d9
jonasdiaz-netizen <jonas.diaz@hotmail.de>
2026-09-03 16:35:46 +0200

    Bump cache for longer sync timeout

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "portfolio-os-shell-";
-const CACHE = `${CACHE_PREFIX}v25-long-term-dividend-growth`;
+const CACHE = `${CACHE_PREFIX}v26-longer-sheet-sync-timeout`;
 const ASSETS = [
   "./index.html",
   "./manifest.webmanifest",
